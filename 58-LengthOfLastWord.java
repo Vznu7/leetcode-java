0ab9aa1bdcfd4684c2758514a6d1b82cea94b03a
vznu7 <vishnuvishnu49373@gmail.com>
2025-02-13 22:45:13 +0530

    length of last word

---
+++ 58-LengthOfLastWord.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+         String[] words = s.split("\\s+");
+        return words.length > 0 ? words[words.length - 1].length() : 0;
+          
+
+        
+    }
+}
